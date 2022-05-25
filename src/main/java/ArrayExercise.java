eb117eded88140a316d97d3ebee9f8d50c5be4df
ayushi1411 <bknashi@gmail.com>
2022-05-25 21:47:14 +0530

    exercise files

---
+++ src/main/java/ArrayExercise.java
@@ -0,0 +1,12 @@
+public class ArrayExercise {
+
+    public void arrayToUpperCase(String[] input){
+        for(int i = 0; i < input.length; i++){
+            input[i] = input[i].toUpperCase();
+        }
+    }
+
+    public void incrementArray(int[] input){
+
+    }
+}
